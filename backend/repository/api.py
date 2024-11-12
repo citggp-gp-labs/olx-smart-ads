d3bf003c22197ca4e506c31bb8f20a1f9fa05a1b
joaohssilva <joaohss1@hotmail.com>
2024-11-12 10:30:15 -0300

    refactors api class and adds addtional line to gemini_image

--- backend/repository/api.py
+++ backend/repository/api.py
@@ -1,28 +1,18 @@
-from app.services.models import GeminiPro
 import requests
-import os
-from app.services.models_image import GeminiImage
 
 class api:
     def __init__(self):
-        self.API_URL = "https://ck-gw.olx.com.br/categories/simple"
+        self.api_url = "https://ck-gw.olx.com.br/categories/simple"
         self.categories = []
-        self.gemini = None
-        self.gemini_image = None
-        self.run()
+        self.fetch_categories()
 
     def fetch_categories(self):
-        response = requests.get(self.API_URL)
-        if response.status_code == 200:
-            self.categories = response.json()
-        else:
-            #todo - implementaion of logging logic
-            #logging.error(f"Error calling the API: {response.status_code}")
-
-    def initialize_gemini_models(self):
-        self.gemini = GeminiPro(project=os.getenv('PROJECT_ID'), location=os.getenv('PROJECT_LOCATION'), model=os.getenv('DEFAULT_MODEL_VERSION'))
-        self.gemini_image = GeminiImage(project=os.getenv('PROJECT_ID'), location=os.getenv('PROJECT_LOCATION'), model=os.getenv('DEFAULT_MODEL_VERSION'))
-
-    def run(self):
-        self.fetch_categories()
-        self.initialize_gemini_models()
+        """Fetch categories from API."""
+        try:
+            response = requests.get(self.api_url, timeout=10)
+            if response.status_code == 200:
+                self.categories = response.json()
+            else:
+                print(f"Error calling the API: {response.status_code}")
+        except requests.exceptions.Timeout:
+            print("API call timed out")
